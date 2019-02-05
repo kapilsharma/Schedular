7681ff10314d15cefc91c88e7e39f51b379fe5b8
Kapil Sharma <kapil@kapilsharma.info>
2019-02-05 13:02:57 +0530

    Project initialization and defining modules - meeting module defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TrainingComponent},
+  { path: 'meeting', loadChildren: './meeting/meeting.module#MeetingModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
